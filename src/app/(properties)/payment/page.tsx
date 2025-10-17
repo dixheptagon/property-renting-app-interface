8044b070f190f3775fedad042d54e23c136a0780
dixheptagon <forentino006@gmail.com>
2025-10-17 21:53:17 +0700

    feat(payment): implement PaymentForm component with cancellation dialog

--- src/app/(properties)/payment/page.tsx
+++ src/app/(properties)/payment/page.tsx
@@ -1,13 +1,11 @@
 "use client";
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import PaymentNavbar from "./_components/payment.navbar";
 import PaymentSummary from "./_components/payment.summary";
 import { LoadingState } from "./_components/loading.and.error";
-import { ImageUp } from "lucide-react";
-import UploadPaymentProof from "./_components/upload.payment.proof";
 import PaymentProofUpload from "./_components/upload.payment.proof";
 import { useRouter } from "next/navigation";
-import { useBookingStore } from "../_stores/booking.store";
+import PaymentForm from "./_components/payment.form";
 
 export default function Page() {
   const router = useRouter();
@@ -15,6 +13,9 @@ export default function Page() {
     <div className="mt-20 mb-10 min-h-full">
       <PaymentNavbar />
       <div className="mx-auto mt-8 max-w-7xl md:grid md:grid-cols-12 md:gap-6">
+        <div className="md:col-span-8">
+          <PaymentForm />
+        </div>
         <div className="md:col-span-4">
           <Suspense fallback={<LoadingState />}>
             <PaymentSummary />
